f3b7f38afffb2c34a963efafafb44c48ecab2522
jhinds <jhinds2010@gmail.com>
2015-09-07 13:24:59 -0500

    used different library for YouTube service

--- src/main/java/com/jhinds/musync/service/YouTubeMusyncService.java
+++ src/main/java/com/jhinds/musync/service/YouTubeMusyncService.java
@@ -1,7 +1,10 @@
 package com.jhinds.musync.service;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.util.Lists;
+import com.google.api.services.youtube.model.PlaylistSnippet;
+import com.google.api.services.youtube.model.PlaylistStatus;
+import com.google.api.services.youtube.model.Playlist;
+import com.google.common.collect.Lists;
 import com.google.api.services.youtube.YouTube;
 import com.google.gdata.client.youtube.YouTubeService;
 import com.google.gdata.data.PlainTextConstruct;
@@ -16,6 +19,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.jhinds.musync.utils.*;
+
 /**
  * Created by jonathanhinds on 9/6/15.
  */
@@ -26,38 +31,34 @@ public class YouTubeMusyncService {
 
 
 
-    public void create() {
+    public void create(String title, String privacyStatus) throws Exception {
         List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube");
+        Credential credential = Auth.authorize(scopes, "playlistupdates");
+
+        youTube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, credential)
+                .setApplicationName("Musync")
+                .build();
+
+        String playlistId = insertPlaylist(title, privacyStatus);
 
     }
 
+    private String insertPlaylist(String title, String privacyStatus) throws Exception {
+        PlaylistSnippet playlistSnippet = new PlaylistSnippet();
+        playlistSnippet.setTitle(title);
+        PlaylistStatus playlistStatus = new PlaylistStatus();
+        playlistStatus.setPrivacyStatus(privacyStatus);
 
+        Playlist youTubePlaylist = new Playlist();
+        youTubePlaylist.setSnippet(playlistSnippet);
+        youTubePlaylist.setStatus(playlistStatus);
 
+        YouTube.Playlists.Insert playlistInsertion = youTube.playlists().insert("snippet,status", youTubePlaylist);
+        Playlist playlistInserted = playlistInsertion.execute();
 
-    YouTubeService service = new YouTubeService(System.getenv("YOUTUBE_CLIENT_ID"), System.getenv("YOUTUBE_DEVELOPER_KEY"));
-    String feedUrl = "http://gdata.youtube.com/feeds/api/users/default/playlists";
-
-    public void createPlaylist(String title) throws IOException, ServiceException {
-        PlaylistLinkEntry playlistLinkEntry = new PlaylistLinkEntry();
-        playlistLinkEntry.setTitle(new PlainTextConstruct(title));
-
-        PlaylistLinkEntry createdEntry = service.insert(new URL(feedUrl), playlistLinkEntry);
+        return  playlistInserted.getId();
     }
 
-    public void addVideoToPlaylist() throws IOException, ServiceException {
-        PlaylistLinkEntry playlistLinkEntry = new PlaylistLinkEntry();
-        String playlistUrl = playlistLinkEntry.getFeedUrl();
-        String videoEntryUrl = "http://gdata.youtube.com/feeds/api/videos/ADos_xW4_J0";
-        VideoEntry videoEntry = service.getEntry(new URL(videoEntryUrl), VideoEntry.class);
-
-        PlaylistEntry playlistEntry = new PlaylistEntry(videoEntry);
-        service.insert(new URL(playlistUrl), playlistEntry);
-    }
-
-    public void removeVideoFromPlaylist() throws IOException, ServiceException {
-        PlaylistLinkEntry playlistLinkEntry = new PlaylistLinkEntry();
-        playlistLinkEntry.delete();
-    }
 
 
 
